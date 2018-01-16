bd9798f1ee6c1216de4f99c3f77902763220d250
Attila <Botz>
2018-01-16 22:21:44 +0100

    Add project files.

---
+++ GUIDO/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using GUIDO.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUIDO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void convertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.convertBtn.IsEnabled = false;
+            this.HideAndClearError();
+
+            string rawText = this.simlpleGuidInput.Text;
+            if (Guid.TryParse(rawText, out Guid parsedGuid))
+            {
+                this.simpleFormGuidBox.Text = parsedGuid.ToString("N");
+                this.properFormGuidBox.Text = parsedGuid.ToString();
+            }
+            else
+            {
+                this.ShowError(GuidoResource.InvalidGuid);
+            }
+            this.convertBtn.IsEnabled = true;
+        }
+
+        private void simlpleGuidInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.HideAndClearError();
+        }
+
+        private void HideAndClearError()
+        {
+            if (this.simpleErrorLabel != null && this.simpleErrorLabel.Visibility == Visibility.Visible)
+            {
+                this.simpleErrorLabel.Visibility = Visibility.Collapsed;
+                this.simpleErrorLabel.Content = string.Empty;
+            }
+        }
+
+        private void ShowError(string error)
+        {
+            this.simpleErrorLabel.Visibility = Visibility.Visible;
+            this.simpleErrorLabel.Content = error;
+        }
+    }
+}
